72753f58066759727ea14b29c9eccb3e2f2f3e69
Mizanur7464 <mdmizanurrahman1860@gmail.com>
2025-07-09 00:41:14 +0600

    Update config.py to use environment variables

--- wellthex_bot/config.py
+++ wellthex_bot/config.py
@@ -1,17 +1,21 @@
-BOT_TOKEN = "8068302044:AAEP7ofyaLhXNudj2m1zqN6fgPTaaG7qG8k"
-ADMIN_ID = 826832942  # আপনার এডমিন আইডি এখানে দিন 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
 # Social and external links
-TWITTER_LINK = "https://x.com/wellthex97"
-INSTAGRAM_LINK = "https://shorturl.at/eyQ8A"
-TELEGRAM_GROUP_LINK = "https://t.me/wellthexofficialgroup"
-TELEGRAM_GROUP_ID = -1002481307874  # Your group chat ID
-YOUTUBE_LINK = "https://www.youtube.com/@wellthex"
-TELEGRAM_CHANNEL_LINK = "https://t.me/wellthex"
-TELEGRAM_CHANNEL_ID = -1002667637660  # Wellthex Official Channel
+TWITTER_LINK = os.getenv("TWITTER_LINK")
+INSTAGRAM_LINK = os.getenv("INSTAGRAM_LINK")
+TELEGRAM_GROUP_LINK = os.getenv("TELEGRAM_GROUP_LINK")
+TELEGRAM_GROUP_ID = int(os.getenv("TELEGRAM_GROUP_ID", "0"))
+YOUTUBE_LINK = os.getenv("YOUTUBE_LINK")
+TELEGRAM_CHANNEL_LINK = os.getenv("TELEGRAM_CHANNEL_LINK")
+TELEGRAM_CHANNEL_ID = int(os.getenv("TELEGRAM_CHANNEL_ID", "0"))
 
 # Token and project info
-TOKEN_NAME = "WTX"
-REWARD_LINK = "https://wellthex.com"
+TOKEN_NAME = os.getenv("TOKEN_NAME", "WTX")
+REWARD_LINK = os.getenv("REWARD_LINK")
 
 # Add more config variables as needed 
